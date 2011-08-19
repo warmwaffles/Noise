f7be5e45bfb96c8b26b0f7859891f9f9e72e0058
Matthew Johnston <warmwaffles@gmail.com>
2011-08-19 15:10:34 -0500

    Finished with testing. On to documenting

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 import riven.PerlinNoise;
 
-
+/**
+ * Just testing out the noise generators
+ * 
+ * @author Matthew A. Johnston (WarmWaffles)
+ *
+ */
 public class Main {
 	public static void main(String args[]) {
 		PerlinNoise p = new PerlinNoise(254);
-		for(float y = -1; y< 1; y += .01) {
-			for(float x = -1; x<1; x += .01) {
+		for(float y = 0; y< 2; y += .01) {
+			for(float x = 0; x<2; x += .01)
 				System.out.print(p.noise(x, y, 0) + "\t");
-			}
 			System.out.println();
 		}
 	}
